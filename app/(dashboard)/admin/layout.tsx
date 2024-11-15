0ac081d6d1da96f4e2931ab43278ecda99be9a7c
Byrone Kinsly <Kinslybyrone17@gmail.com>
2024-11-15 09:46:07 +0300

    updated the admin dashboard for better UI/UX

--- app/(dashboard)/admin/layout.tsx
+++ app/(dashboard)/admin/layout.tsx
@@ -6,9 +6,9 @@ export default function AdminLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="flex min-h-screen">
+    <div className="flex h-[80vh] overflow-hidden">
       <AdminSidebar />
-      <main className="flex-1 p-8">{children}</main>
+      <main className="flex-1 p-8 h-full overflow-y-auto">{children}</main>
     </div>
   );
 }
--- components/admin/sidebar.tsx
+++ components/admin/sidebar.tsx
@@ -62,7 +62,7 @@ export function AdminSidebar() {
   const pathname = usePathname();
 
   return (
-    <div className="space-y-4 py-4 flex flex-col h-full bg-slate-900 text-white">
+    <div className="space-y-4 py-4 flex flex-col h-screen  bg-slate-900 text-white top-0 bottom-0 sticky">
       <div className="px-3 py-2 flex-1">
         <Link href="/admin" className="flex items-center pl-3 mb-14">
           <h1 className="text-2xl font-bold">Admin Panel</h1>
